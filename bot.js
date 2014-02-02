17eaa134502d4d1dbf2a1282195c14be4dee3f50
Sgt. Kabukiman <sgt@kabukiman.org>
2014-02-02 05:14:06 +0100

    starting to work on the basic bot logic (the core plugin), handling moderator and subscriber states and stuff

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 var
 	TwitchClient = require('./lib/TwitchClient.js'),
 	Channel      = require('./lib/Channel.js'),
+	Core         = require('./lib/Plugin/Core.js'),
 	Dummy        = require('./lib/Plugin/ConsoleOutput.js'),
 	irc          = require('irc'),
 	sqlite3      = require('sqlite3');
@@ -20,9 +21,9 @@ var db = new sqlite3.Database('test.sqlite3', sqlite3.OPEN_READWRITE | sqlite3.O
 });
 
 var twitchClient = new TwitchClient(ircClient);
-var dummy        = new Dummy(console);
 
-twitchClient.addPlugin(dummy);
+twitchClient.addPlugin(new Core(db));
+twitchClient.addPlugin(new Dummy(console));
 
 twitchClient.connect([
 	new Channel('...')
