fc466d5f7ee0a9ebef5bfc0dcefaca53049bee77
Sgt. Kabukiman <sgt@kabukiman.org>
2014-02-02 02:34:18 +0100

    first, very rough draft; still experimenting with the Twitch IRC protocol

---
+++ bot.js
@@ -0,0 +1,25 @@
+var
+	TwitchClient = require('./lib/TwitchClient.js'),
+	Channel      = require('./lib/Channel.js'),
+	irc          = require('irc'),
+	sqlite3      = require('sqlite3');
+
+var ircClient = new irc.Client('irc.twitch.tv', '...', {
+	showErrors: true,
+	autoConnect: false,
+	floodProtection: true,
+	floodProtectionDelay: 1000, // 1 sec
+	password: '...',
+	realName: '...',
+	debug: false
+});
+
+var db = new sqlite3.Database('test.sqlite3', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function() {
+	// db.run('CREATE TABLE IF NOT EXISTS channels (name VARCHAR(200), ')
+});
+
+var twitchClient = new TwitchClient(ircClient);
+
+twitchClient.connect([
+	new Channel('...')
+]);
