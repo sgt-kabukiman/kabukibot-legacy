aec5db86ad53e0dadd8be37b9d48352169b281d9
Sgt. Kabukiman <sgt@kabukiman.org>
2014-03-09 00:43:23 +0100

    introduced a variable command prefix for the global commands, so it's easier to run Kabukit under a different account

--- config.dist.js
+++ config.dist.js
@@ -25,6 +25,13 @@ module.exports = {
 	// permissions, created commands etc.
 	database: 'kabukibot.sqlite3',
 
+	// command prefix
+	// This is the prefix that is put in front of all global commands, like ![prefix]join
+	// or ![prefix]allow. Custom commands and those provided by content packs are not affected.
+	// This is because the owner of a channel actively opted-in to have those commands in their
+	// channels, compared to global commands a broadcaster cannot defend against.
+	commandPrefix: 'mybot_',
+
 	// internal values
 	// In general there is no reason to change these.
 
