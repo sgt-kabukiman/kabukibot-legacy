73a579162ee223a6b71911be515274d879abd01e
Sgt. Kabukiman <sgt@kabukiman.org>
2014-05-24 00:56:23 +0200

    first baby steps in having a few unit tests

---
+++ tests/TwitchMessage.js
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2014, Sgt. Kabukiman, https://bitbucket.org/sgt-kabukiman/
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+var TM = require('./../lib/TwitchMessage.js');
+
+function assertMessage(test, msg, chan, cmd, args, username) {
+	test.deepEqual(msg.getChannel(),  chan);
+	test.deepEqual(msg.getCommand(),  cmd);
+	test.deepEqual(msg.getArgs(),     args);
+	test.deepEqual(msg.getUsername(), username);
+}
+
+exports.testParseClearchat = function(test) {
+	var msg = TM.fromIrcMessage(null, 'CLEARCHAT');
+	assertMessage(test, msg, null, 'clearchat', [], null);
+
+	test.done();
+};
+
+exports.testParseTimeout = function(test) {
+	var msg = TM.fromIrcMessage(null, 'CLEARCHAT a_username');
+	assertMessage(test, msg, null, 'clearchat', ['a_username'], 'a_username');
+
+	test.done();
+};
+
+exports.testParseSpecialuser = function(test) {
+	var msg = TM.fromIrcMessage(null, 'SPECIALUSER a_username subscriber');
+	assertMessage(test, msg, null, 'specialuser', ['a_username', 'subscriber'], 'a_username');
+
+	test.done();
+};
+
+exports.testParseUsercolor = function(test) {
+	var msg = TM.fromIrcMessage(null, 'USERCOLOR a_username #123456');
+	assertMessage(test, msg, null, 'usercolor', ['a_username', '#123456'], 'a_username');
+
+	test.done();
+};
+
+exports.testParseEmoteset = function(test) {
+	var msg = TM.fromIrcMessage(null, 'EMOTESET a_username [12,34,56]');
+	assertMessage(test, msg, null, 'emoteset', ['a_username', '[12,34,56]'], 'a_username');
+
+	test.done();
+};
+
+exports.testParseUnknown = function(test) {
+	// only the first 2 arguments are relevant and being parsed
+	var msg = TM.fromIrcMessage(null, 'FUNKY a_username #123456 something_else foo,bar');
+	assertMessage(test, msg, null, 'funky', ['a_username', '#123456'], null);
+
+	test.done();
+};
---
+++ tests/Utils.js
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2014, Sgt. Kabukiman, https://bitbucket.org/sgt-kabukiman/
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+var Utils = require('./../lib/Utils.js');
+
+exports.testDateToSQL = function(test) {
+	test.deepEqual(Utils.dateToSQL(new Date('2014-01-01 UTC')),          '2014-01-01 00:00:00');
+	test.deepEqual(Utils.dateToSQL(new Date('2014-01-01 01:20:07 UTC')), '2014-01-01 01:20:07');
+	test.done();
+};
+
+exports.testHumanJoin = function(test) {
+	var tests = [
+		[             [],           ''],
+		[          ['a'],          'a'],
+		[     ['a', 'b'],    'a and b'],
+		[['a', 'b', 'c'], 'a, b and c']
+	];
+
+	for (var i = 0; i < tests.length; ++i) {
+		test.deepEqual(Utils.humanJoin(tests[i][0]), tests[i][1]);
+	}
+
+	test.done();
+};
+
+exports.testIsEmptyObject = function(test) {
+	var tests = [
+		[         {},       true],
+		[   {foo: 1},      false],
+		[{foo: null},      false],
+		[{foo: undefined}, false]
+	];
+
+	for (var i = 0; i < tests.length; ++i) {
+		test.deepEqual(Utils.isEmptyObject(tests[i][0]), tests[i][1]);
+	}
+
+	test.done();
+};
+
+exports.testGetObjectKeys = function(test) {
+	var tests = [
+		[              {},             []],
+		[        {foo: 1},        ['foo']],
+		[{foo: 1, bar: 2}, ['foo', 'bar']]
+	];
+
+	for (var i = 0; i < tests.length; ++i) {
+		test.deepEqual(Utils.getObjectKeys(tests[i][0]), tests[i][1]);
+	}
+
+	test.done();
+};
+
+exports.testRandomItem = function(test) {
+	var tests = [
+		[      {},  null],
+		[      [],  null],
+		[{foo: 1}, 'foo'],
+		[   ['a'],   'a']
+	];
+
+	for (var i = 0; i < tests.length; ++i) {
+		test.deepEqual(Utils.randomItem(tests[i][0]), tests[i][1]);
+	}
+
+	test.done();
+};
+
+exports.testSecondsToTime = function(test) {
+	var tests = [
+		[       0,                     '',                                           ''],
+		[       1,                  '01s',                                   '1 second'],
+		[       2,                  '02s',                                  '2 seconds'],
+		[      59,                  '59s',                                 '59 seconds'],
+		[    59.5,                  '59s',                                 '59 seconds'], // properly truncate flots?
+		[      60,              '01m:00s',                                   '1 minute'],
+		[     122,              '02m:02s',                    '2 minutes and 2 seconds'],
+		[  521100,      '06d:00h:45m:00s',                      '6 days and 45 minutes'],
+		[  528300,      '06d:02h:45m:00s',             '6 days, 2 hours and 45 minutes'],
+		[61613100, '101w:06d:02h:45m:00s',  '101 weeks, 6 days, 2 hours and 45 minutes']  // do not go into months and years, because months have different number of days
+	];
+
+	for (var i = 0; i < tests.length; ++i) {
+		test.deepEqual(Utils.secondsToTime(tests[i][0], true), tests[i][1]);
+		test.deepEqual(Utils.secondsToTime(tests[i][0], false), tests[i][2]);
+	}
+
+	test.done();
+};
+
+exports.testSecondsToRunTime = function(test) {
+	var tests = [
+		[    1,       '1'],
+		[  122,    '2:02'],
+		[21900, '6:05:00']
+	];
+
+	for (var i = 0; i < tests.length; ++i) {
+		test.deepEqual(Utils.secondsToRunTime(tests[i][0]), tests[i][1]);
+	}
+
+	test.done();
+};
